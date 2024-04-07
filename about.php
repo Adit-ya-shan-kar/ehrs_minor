5cf6928fc4a6550bc016e97829846772c5da6529
Aditya Shankar <94442693+Adit-ya-shan-kar@users.noreply.github.com>
2024-04-07 22:21:43 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,61 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Ups</h5>
+                            <p>Take proactive steps towards your health with our comprehensive health check-up service.
+                                From routine screenings to in-depth assessments, our expert team ensures a thorough evaluation
+                                of your well-being. With state-of-the-art facilities and personalized care, we provide detailed
+                                insights and guidance tailored to your unique health needs. Invest in your wellness today and 
+                                embark on a journey towards a healthier, happier life with our trusted health check-up service.</p>
+                            <ul class="row">
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                    <p>Rapid, expert care, ensuring life-saving treatment
+                                        during critical moments of need. </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                    <p>A team of highly skilled doctors, dedicated to 
+                                        delivering exceptional care with expertise and compassion. </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                    <p>Effortlessly schedule your medical appointments online with our 
+                                        convenient and user-friendly hospital appointment service. </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                    <p>Access professional guidance and support with our complimentary 
+                                        medical counseling service, offering confidential assistance to all.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
